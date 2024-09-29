f6d76716c33332fe874af90ff587ba91ac235b1c
louislat <lmm.latournerie@gmail.com>
2024-09-29 15:33:00 +0200

    proof of work

--- data/generate_data.jl
+++ data/generate_data.jl
@@ -1,9 +1,14 @@
 
 
 function generate_easy_data(n::Int32)::Tuple{Array{Float32}, Array{Float32}}
-	X::Array{Float32} = randn(n, 10)
-	y::Array{Float32} = 1 ./ (1 .+ exp.(.-X[:, 5])) .+ 0.1 * randn(n)
+	X::Array{Float32} = randn(n, 1)
+	y::Array{Float32} = 1 ./ (1 .+ exp.(.-X[:, 1])) + 0.05 .* randn(n)
 	return X, y
+end
+
+function generate_easy_data(X::Array{Float32})::Array{Float32}
+	y::Array{Float32} = 1 ./ (1 .+ exp.(.-X[:, 1])) + 0.05 .* randn(size(X)[1])
+	return y
 end
 
 function generate_complex_data(n::Int32)::Tuple{Array{Float32}, Array{Float32}}
--- main.jl
+++ main.jl
@@ -1,14 +1,14 @@
 using Printf
 using Plots
 include("data/generate_data.jl")
-include("tree/larger_tree.jl")
+include("tree/tree_regressor.jl")
 
 println("Generating data ...")
-X, y = generate_complex_data(Int32(10000))
+X, y = generate_easy_data(Int32(10000))
 println("Done!")
 
 println("Initializing tree ...")
-tree::DecisionTree = DecisionTree(50, 3, 2, nothing)
+tree::DecisionTree = DecisionTree(50, 3, 1, nothing)
 println("Done!")
 
 println("Training tree ...")
@@ -16,9 +16,11 @@ fit(tree, X = X, y = y)
 println("Done!")
 
 println("Predict ...")
-X_test, y_test = generate_complex_data(Int32(1000))
+# X_test, y_test = generate_easy_data(Int32(1000))
+X_test::Array{Float32} = reshape(LinRange(-3, 3, 1000), 1000, 1)
+y_test = generate_easy_data(X_test)
 y_pred = predict(tree, X_test)
 println("Done!")
 
-plot(X_test[:, 1] - X_test[:, 2], y_test, seriestype = :scatter)
-plot!(X_test[:, 1] - X_test[:, 2], y_pred, seriestype = :scatter)
+scatter(X_test[:, 1], y_test, label = "train set")
+plot!(X_test[:, 1], y_pred, lw = 3, label = "model prediction")
